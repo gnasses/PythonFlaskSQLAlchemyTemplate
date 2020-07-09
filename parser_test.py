2b5ae6630603479663bbb277ace1214f5d79c77a
Nasses, George W <george.nasses@optum.com>
2020-07-09 14:53:02 -0500

    Update parser_test.py

--- parser_test.py
+++ parser_test.py
@@ -3,9 +3,25 @@ from netmiko import Netmiko
 from ntc_templates.parse import parse_output
 print ("Device Name: ")
 hostname = input()
+#hostname = "mlselr01ccm13"
+#hostname = "mls01elr56pri01"
+print ("Command to test parsing: ")
+cmd = input()
 device = util.CiscoDeviceRO(host=hostname)
 net_connect = Netmiko(**device.__dict__)
-out = net_connect.send_command("show version")
-out_parsed = parse_output(platform="cisco_ios", command="show version", data=out)
-#out_parsed = parse_output(platform="cisco_nxos", command="show version", data=out)
+out = net_connect.send_command(cmd)
+sw_ver = util.nxos_check(device)
+if sw_ver == "nxos":
+    out_parsed = parse_output(platform="cisco_ios", command=cmd, data=out)
+else:
+    out_parsed = parse_output(platform="cisco_nxos", command=cmd, data=out)
 print(out_parsed)
+
+"""
+#Sample Commands to Test in both NXOS/IOS
+show version
+show inventory 
+show cdp neighbors
+show environment temperature
+show ip interface brief
+"""
